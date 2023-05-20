f359faf70731353bdd404e11adc2886ef311efd6
orderyo <markovmaks895@gmail.com>
2023-05-20 14:42:46 +0300

    замена регулярного выражения

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println(ip);
     }
     public static String examinationIp(String in){
-        Pattern pattern = Pattern.compile("(.*)(((0|[0-1]([0-9]{2})|2[0-4][0-9]|25[0-5])\\.){2}\\d\\.\\d)(.*)");
+        Pattern pattern = Pattern.compile("(.*)((([0-9]|[1-9][0-9]|1([0-9]{2})|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1([0-9]{2})|2[0-4][0-9]|25[0-5]))(.*)");
         Matcher matcher = pattern.matcher(in);
         String ip = null;
         if (matcher.find()){
